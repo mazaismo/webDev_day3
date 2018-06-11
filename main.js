028a02eb792a33cb6bd5b9c4fb74f5a3a19421a0
Modris Ābrams <brainstormer57@googlemail.com>
2018-06-11 20:29:12 +0300

    1

---
+++ main.js
@@ -0,0 +1,33 @@
+var someString1 = "foo";
+var someString2 = 'foo';
+var someString3 = "foo";
+
+var someInteger = 123;
+var someFloat = 3.14;
+var someBool = true;
+var someArray = ['vaz','Zaz','Gaz','BMW', 123, 3.14, true];
+var someNull = null;
+
+ 
+var somePerson =
+
+    {
+    name: "johhny",
+    surname: "Gates",
+    age: 16,
+
+};
+var people =
+{
+    name: "johhny",
+    surname: "Gates",
+    age: 16,
+
+};
+
+var doSomething = function(){
+
+}
+
+
+
